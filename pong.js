78069769d1ec1e70cd083681e803520f06020a95
Fernando S Medeiros <fernando.medeiros.ti@gmail.com>
2017-12-03 20:31:32 -0300

    inicio

---
+++ pong.js
@@ -0,0 +1,11 @@
+function jogar(){
+	var div = $("#bola")			
+	div.animate({top:'295px', left:'395px'}, "slow");
+	
+	console.log((-3) * Math.PI / 4);	
+}
+
+function rand(){
+	return Math.random()	
+}
+	
