d6ccc32d0e47a21112ad67acd66f95c20b3ce5c7
Fernando S Medeiros <fernando.medeiros.ti@gmail.com>
2017-12-05 16:27:38 -0300

    agora vai

--- pong.js
+++ pong.js
@@ -1,11 +1,5 @@
-function jogar(){
-	var div = $("#bola")			
-	div.animate({top:'295px', left:'395px'}, "slow");
+$(function(){		
+		
 	
-	console.log((-3) * Math.PI / 4);	
-}
-
-function rand(){
-	return Math.random()	
-}
+})
 	
