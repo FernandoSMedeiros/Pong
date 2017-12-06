ce1b93ee05a26f6018703986c86e1fef8026d105
Fernando S. Medeiros <fernando.medeiros.ti@gmail.com>
2017-12-06 20:29:18 -0300

    A pedido do professor
     Foi criado a barra dfe life, deixado como jogador único, darggable, e
     com alert de game-over.

--- javascript/pong.js
+++ javascript/pong.js
@@ -1,17 +1,22 @@
 $(function(){
 	var bola = $("#bola");
+	var barraVida = $("#barraVida");
+	var barraVidaTamanho = barraVida.width();
 	var x = bola.position().left;
 	var y = bola.position().top;
 
-	console.log("riwuhfgruyhiuew".x)
+	var movimentacao = 1;
 
-	var direcaoX = 1;
-	var direcaoY = 1;
+	var direcaoX = movimentacao;
+	var direcaoY = movimentacao;
 
 	var tela  = $("#fundo");
 	var limiteTela = new Object()
 	var jogador1 = $("#jogador1");
 	var jogador2 = $("#jogador2");
+
+	jogador1.draggable({axis:"y", containment:"#fundo"});
+	bola.draggable({containment:"#fundo"});
 
 	var pontosJogador1 = 0;
 	var pontosJogador2 = 0;
@@ -19,13 +24,20 @@ $(function(){
 	jogador1.css("left", 50);
 	jogador2.css("left", 750);
 
-	limiteTela.top = tela.offset().top;
-	limiteTela.bottom = tela.offset().top + tela.height();
-	limiteTela.left = tela.offset().left;
-	limiteTela.right = tela.offset().left + tela.width();
+	var isGamb = false;
 
 	function jogar(){
 		
+
+		if(pontosJogador1%2==0){
+			if(isGamb){
+				movimentacao+=0.5;
+				isGamb = false;
+			}
+
+		}else{
+			isGamb = true;
+		}
 		
 		x += direcaoX;
 		y += direcaoY;
@@ -35,30 +47,39 @@ $(function(){
 
 
 		if(bola.position().top+(bola.height()/4)>= tela.height()){
-			direcaoY = -1;
+			direcaoY = -(movimentacao);
 		}
 		else if(bola.offset().top -(bola.width()/4)<= tela.offset().top){
-			direcaoY = 1;
+			direcaoY = movimentacao;
 		}
-		else if(bola.position().left+(bola.width()/4)== tela.width()){
-			x = 395;
+		else if(bola.position().left+(bola.width()/4)>= tela.width()){
+			/*x = 395;
 			y = 255;
 			pontosJogador1++;
-			document.getElementById("player1").innerHTML = pontosJogador1;
+			document.getElementById("player1").innerHTML = pontosJogador1;*/
+			direcaoX = -(movimentacao);
 
 		}
-		else if(bola.offset().left -(bola.width()/4) == tela.offset().left){
+		else if(bola.offset().left -(bola.width()/4) <= tela.offset().left){
 			x = 395;
 			y = 255;
+			barraVida.css("width",barraVida.width()-(barraVidaTamanho*0.2));
 			pontosJogador2++;
 			document.getElementById("player2").innerHTML = pontosJogador2;
 
 		}
 		else if (isColisaoJogador1()) {
-			direcaoX = 1;
+			direcaoX = movimentacao;
+			pontosJogador1++;
+			document.getElementById("player1").innerHTML = pontosJogador1;
 		}
-		else if (isColisaoJogador2()) {
-			direcaoX = -1;
+		/*else if (isColisaoJogador2()) {
+			direcaoX = -(movimentacao);
+		}*/
+
+		if(barraVida.width()<=10){
+			alert("Perdeu, playboy!");
+			location.reload();
 		}
 		
 		window.addEventListener('keydown',botaoApertado,true);
@@ -66,7 +87,7 @@ $(function(){
 
 
 	function isColisaoJogador1(){
-		var res = (bola.position().left+(bola.width()/2)-10 == jogador1.position().left) && (bola.position().top >= jogador1.position().top && bola.position().top <= jogador1.position().top + jogador1.height());
+		var res = (bola.position().left+(bola.width()/2)-10 <= jogador1.position().left) && (bola.position().top >= jogador1.position().top && bola.position().top <= jogador1.position().top + jogador1.height());
 				
 		return (res);
 	}
