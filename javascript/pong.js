3a3e1334d83c879feaeffdba6c01d8476f838958
Fernando S. Medeiros <fernando.medeiros.ti@gmail.com>
2017-12-06 15:31:28 -0300

    pontuação e linha ok

--- javascript/pong.js
+++ javascript/pong.js
@@ -1,5 +1,4 @@
-$(function(){		
-	
+$(function(){
 	var bola = $("#bola");
 	var x = bola.position().left;
 	var y = bola.position().top;
@@ -13,6 +12,10 @@ $(function(){
 	var limiteTela = new Object()
 	var jogador1 = $("#jogador1");
 	var jogador2 = $("#jogador2");
+
+	var pontosJogador1 = 0;
+	var pontosJogador2 = 0;
+
 	jogador1.css("left", 50);
 	jogador2.css("left", 750);
 
@@ -30,31 +33,38 @@ $(function(){
         bola.css("top", y);
 		bola.css("left", x);
 
-		console.log(x)
 
-        		
-		/*if(bola.position().left+(bola.width()/4)>= tela.width()){
-			clearInterval(loop);
-		}else if(bola.offset().left -(bola.width()/4) <= tela.offset().left){
-			clearInterval(loop);
-		}else */if(bola.position().top+(bola.height()/4)>= tela.height()){
+		if(bola.position().top+(bola.height()/4)>= tela.height()){
 			direcaoY = -1;
 		}
 		else if(bola.offset().top -(bola.width()/4)<= tela.offset().top){
 			direcaoY = 1;
 		}
-		else if(bola.position().left+(bola.width()/4)>= tela.width()){
-			direcaoX = -1;
-		}else if(bola.offset().left -(bola.width()/4) <= tela.offset().left){
+		else if(bola.position().left+(bola.width()/4)== tela.width()){
+			x = 395;
+			y = 255;
+			pontosJogador1++;
+			document.getElementById("player1").innerHTML = pontosJogador1;
+
+		}
+		else if(bola.offset().left -(bola.width()/4) == tela.offset().left){
+			x = 395;
+			y = 255;
+			pontosJogador2++;
+			document.getElementById("player2").innerHTML = pontosJogador2;
+
+		}
+		else if (isColisaoJogador1()) {
 			direcaoX = 1;
-		}else if (isColisaoJogador1()) {
-			direcaoX = 1;
-		}else if (isColisaoJogador2()) {
+		}
+		else if (isColisaoJogador2()) {
 			direcaoX = -1;
 		}
 		
 		window.addEventListener('keydown',botaoApertado,true);
 	}
+
+
 	function isColisaoJogador1(){
 		var res = (bola.position().left-(bola.width()/4) == jogador1.position().left) && (bola.position().top >= jogador1.position().top && bola.position().top <= jogador1.position().top + jogador1.height());
 		return res;
@@ -82,10 +92,8 @@ $(function(){
 			
 		}
 	}
-
 	var loop = setInterval(jogar, 1);
 	
 	
-	
-})
+});
 	
